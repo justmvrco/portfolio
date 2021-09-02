d482298a985918fb07dcdc2c38b10c565e200b14
jusmvrco <moreirapiliki@gmail.com>
2021-09-03 00:11:14 +0200

    marco

---
+++ src/pages/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Layout, Hero, About, Jobs, Featured, Projects, Contact } from '@components';
+
+const StyledMainContainer = styled.main`
+  counter-reset: section;
+`;
+
+const IndexPage = ({ location }) => (
+  <Layout location={location}>
+    <StyledMainContainer className="fillHeight">
+      <Hero />
+      <About />
+      <Jobs />
+      <Featured />
+      <Projects />
+      <Contact />
+    </StyledMainContainer>
+  </Layout>
+);
+
+IndexPage.propTypes = {
+  location: PropTypes.object.isRequired,
+};
+
+export default IndexPage;
+2021 ,GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact, GitHub
+Pricing
+API
+Training
+Blog
+About
+
